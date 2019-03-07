13391e1d9d3d5c7c839393a9f8cf75d105f4625a
Lorenzo Scott Bertolini <39827031+lorenzoscottb@users.noreply.github.com>
2019-03-07 14:06:22 +0000

    Update txt2space.py

--- txt2space.py
+++ txt2space.py
@@ -25,15 +25,25 @@ class Space():
     def key_by_value(self, value):
         return list(self.vocabulary.keys())[list(self.vocabulary.values()).index(value)]
 
-    def txt2space(self, dir, token=None, dimesion=None, en_remove=False, dim_in_file=False):
+    def vulic_lear_txt2space(self):
 
+        file = open('/Users/lb540/Documents/corpora/vectors/lear_hyperlex.txt', 'r')
+
+        self.matrix_space = np.ndarray(shape=(183870, 300))  # (183870, 300)
+
+        line = file.readline()
+        for index, line in enumerate(file):
+            self.vocabulary[line.split(' ', 1)[0]] = index
+            self.matrix_space[index] = np.fromstring(line.split(' ', 1)[1], dtype="float32", sep=" ")
+
+    def txt2space(self, dir, token=None, dimesion=None, en_remove=False, dim_in_file=False):
 
         file = open(dir, 'r')
         line = file.readline()
         if dim_in_file:
             info = line.split()
-            token = int(info[1])+1
-            dimesion = int(info[-1])
+            token = int(info[0])
+            dimesion = int(info[1])
 
         self.matrix_space = np.ndarray(shape=(token, dimesion))
 
@@ -43,7 +53,7 @@ class Space():
             else:
                 self.vocabulary[line.split(' ', 1)[0]] = index
             self.matrix_space[index] = np.fromstring(line.split(' ', 1)[1], dtype="float32", sep=" ")
-            
+
     def to_csr_matrix(self):
         
         import scipy.sparse.to_csr_matrix as csr
@@ -109,7 +119,8 @@ class Space():
             sumxy += x*y
         return sumxy/math.sqrt(sumxx*sumyy)    
 
-    def extract_knn(self, word, n_nbrs=16, en_=True):
+    def extract_knn(self, word, algorithm='brute', n_nbrs=16,
+                    metric='cosine', return_distance=False, en_=True):
 
         """
         word: single word string or array vector (for composed vectors)
@@ -119,7 +130,7 @@ class Space():
         space = self.matrix_space
         mydict = self.vocabulary
 
-        nn = NN(algorithm='brute', n_neighbors=n_nbrs, metric='cosine')
+        nn = NN(algorithm=algorithm, n_neighbors=n_nbrs, metric=metric)
         nn.fit(space)
 
         if not en_:
@@ -134,15 +145,15 @@ class Space():
             vector = [word]
             v1 = word
 
-        word_nn = nn.kneighbors(vector, return_distance=False)
+        word_nn = nn.kneighbors(vector, return_distance=return_distance)
 
         final_knn = []
         for e in word_nn[0]:
-            if self.key_by_value(mydict, e) != word:
+            if self.key_by_value(e) != word:
 
                 v2 = self.matrix_space[e]
 
-                final_knn.append((self.key_by_value(mydict, e), self.cosine_similarity(v1, v2)))
+                final_knn.append((self.key_by_value(e), self.cosine_similarity(v1, v2)))
 
         return final_knn
 
@@ -162,8 +173,8 @@ class Space():
         labels = list(self.vocabulary.keys())[:word_count]
         return _plot_with_labels(low_dim_embs, labels, path, size)
 
-    def ml_10_evaluation(self, test_phrase='adjectivenouns', en=False, plot=False
-                         ml_10 = 'path/to/ml_10.csv'):
+    def ml_10_evaluation(self, test_phrase='adjectivenouns', en=False, plot=False,
+    					 ml_10 = '/Users/lb540/Documents/corpora/similarity/mitchell-lapata/ml_10.csv'):
 
         from scipy.stats.stats import spearmanr
         import pandas as pd
@@ -183,10 +194,9 @@ class Space():
             try:
                 c_1  = self.vector(e[3], en_=en) + self.vector(e[4], en_=en)
                 c_2  = self.vector(e[5], en_=en) + self.vector(e[6], en_=en)
-
-                ml_values.append(int(e[-1]))
                 cs_values.append(self.cosine_similarity(c_1, c_2))
-                print('%s:collected' % e[3:7])
+                ml_values.append(int(e[-1]))
+                # print('%s:collected' % e[3:7])
 
             except:
                 print('%s:something thing whent wrong' % e[3:7])
@@ -201,23 +211,24 @@ class Space():
             sns.set(style="whitegrid")
             plt.scatter(ml_values,cs_values)
 
-    def simlex_evaluation(self,  en=False, plot=False
-                          simlex = 'path/to/SimLex-en.csv'):
+    def simlex_evaluation(self,  en=False, plot=False,
+    					  simlex = '/Users/lb540/Documents/corpora/similarity/SimLex-en.csv'):
 
         from scipy.stats.stats import spearmanr
         import pandas as pd
+
+        
 
         df = pd.read_csv(simlex)
         sim_values = []
         cs_values = []
 
         for index, e in enumerate(df.values):
-            print(e)
             sim_values.append(int(e[-1]))
             cs_values.append(self.cosine_similarity(e[0], e[1]))
             print('%s:evaluated' % e[0:2])
 
-        print(' vectors ', spearmanr(sivalues, cs_values))
+        print(' vectors ', spearmanr(sim_values, cs_values))
 
         if plot:
 
@@ -225,7 +236,7 @@ class Space():
             import seaborn as sns
 
             sns.set(style="whitegrid")
-            plt.scatter(sivalues,cs_values)
+            plt.scatter(sim_values,cs_values)
 
 def _plot_with_labels(low_dim_embs, labels, path, size):
 

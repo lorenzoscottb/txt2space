209f5ee50aa9ced213b2e9b5e300bebade47832d
Lorenzo Scott Bertolini <39827031+lorenzoscottb@users.noreply.github.com>
2019-03-25 20:06:40 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,7 @@
+1 from setuptools import setup
+2 
+3 setup(
+4     name='my-awesome-helloworld-script',    # This is the name of your PyPI-package.
+5     version='0.1',                          # Update the version number for new releases
+6     scripts=['helloworld']                  # The name of your scipt, and also the command you'll be using for calling it
+7 )

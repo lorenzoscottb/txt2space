ffc6351b622ac6a4c04e44d6ba476202e1f8952c
Lorenzo Scott Bertolini <39827031+lorenzoscottb@users.noreply.github.com>
2019-03-25 20:10:37 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ setup(
     name='txt2space',    # This is the name of your PyPI-package.
     version='0.1.2',                          # Update the version number for new releases
 #    scripts=['helloworld']                  # The name of your scipt, and also the command you'll be using for calling it
-    install_requires=['numpy', 'scipy', 'sparsesvd'],
+    install_requires=['numpy', 'sklearn', 'nltk'],
 
 )

4720959bfc99eea5deaa1ec81f37b76a6e1af472
Lorenzo Scott Bertolini <39827031+lorenzoscottb@users.noreply.github.com>
2019-03-26 09:02:03 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
-from setuptools import setup
- 
-setup(
-    name='txt2space',    # This is the name of your PyPI-package.
-    version='0.1.2',                          # Update the version number for new releases
-#    scripts=['helloworld']                  # The name of your scipt, and also the command you'll be using for calling it
-    install_requires=['numpy', 'sklearn', 'nltk'],
+import setuptools
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="txt2space-lorenzoscottb",
+    version="0.1.2",
+    author="Lorenzo Scott Bertolini",
+    author_email="l.bertolini@sussex.ac.uk",
+    description="convert txt file to vector space",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/lorenzoscottb/txt2space",
+    packages=setuptools.find_packages(),
 )
